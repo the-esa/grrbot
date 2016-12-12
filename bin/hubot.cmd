1082e142c6c698c87b4163652d860a12fe268254
the-esa <theresa.altmann@gmx.de>
2016-12-12 17:24:07 +0100

    adding channel id

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -5,6 +5,6 @@ SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 SET HUBOT_DISCORD_TOKEN=MjUxNjUxNTQ5ODA0MTAxNjMy.CzBZig.VtYHtAaCbmYY_2gZ_tUVAqIhSIM
 SET HUBOT_MAX_MESSAGE_LENGTH=2000
-export HUBOT_EVENT_NOTIFIER_ROOM="general"
+export HUBOT_EVENT_NOTIFIER_ROOM="251652594303369218"
 
 node_modules\.bin\hubot.cmd --name "mogai" %* 
